3ebbfb0467c6dc74041d9068d875cb02264eb2ab
enriqueplace <enriqueplace@339f5665-9641-0410-b0e2-e15373425c37>
2007-12-19 02:49:01 +0000

    Primera estructura base para empezar el proyecto

---
+++ trunk/application/modules/usuarios/controllers/UsuariosController.php
@@ -0,0 +1,26 @@
+<?php
+class Usuarios_UsuariosController extends Zend_Controller_Action
+{
+    function init()
+    {
+    }
+    function preDispatch()
+    {
+    }
+    function indexAction()
+    {
+    }
+    function agregarAction()
+    {
+    }
+    function modificarAction()
+    {
+    }
+    function eliminarAction()
+    {
+    }
+    function verAction()
+    {
+    }
+}
+?>
