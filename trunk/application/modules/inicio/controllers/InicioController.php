56ce47f57511029143beacd548584204c3e2cce9
at.elah <at.elah@339f5665-9641-0410-b0e2-e15373425c37>
2007-12-24 16:28:50 +0000

    Modulo de inicio. Bug: no mostraba nada

--- trunk/application/modules/inicio/controllers/InicioController.php
+++ trunk/application/modules/inicio/controllers/InicioController.php
@@ -2,11 +2,14 @@
 class Inicio_InicioController extends Zend_Controller_Action {
     function init()
     {
+        $this->initView();
+        $this->view->baseUrl = $this->_request->getBaseUrl();
+        $this->view->setScriptPath('./application/views/scripts/');
     }
     function preDispatch()
     {
     }
-    function indexAction()
+    function inicioAction()
     {
     }
     function agregarAction()
