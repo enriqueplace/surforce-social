f96447ee13d4f5e11ebd086d704043cb59eb6da9
at.elah <at.elah@339f5665-9641-0410-b0e2-e15373425c37>
2007-12-28 13:18:45 +0000

    Update: Hice unos cambios en la tabla de usuarios. Espero que puedan bajarse la ultima version
    Update: Se actualizo el registro
    Faltan algunos puntos como validar si el usuario y el mail ya existen, y que si hay errores, te vuelva al formulario de registro mostrandote los campos que ya completaste.
    Ademas hay que agregar la funcionalidad para enviar mail.

--- trunk/application/modules/registracion/controllers/RegistracionController.php
+++ trunk/application/modules/registracion/controllers/RegistracionController.php
@@ -2,9 +2,16 @@
 class Registracion_RegistracionController extends Zend_Controller_Action
 {
     public function init(){
-        $this->initView();
-        $this->view->baseUrl = $this->_request->getBaseUrl();
-        $this->view->setScriptPath('./application/views/scripts/');
+		$this->initView();
+		$this->view->baseUrl = $this->_request->getBaseUrl();
+		$this->view->setScriptPath('./application/views/scripts/');
+		Zend_Loader::loadClass('Registracion');
+
+		#factorizando la instancia de 'personalizacion'
+		$this->info = Zend_Registry::get('personalizacion');
+		#asignando el titulo de todo el sitio
+		$this->view->title = $this->info->sitio->index->index->titulo;
+
     }
     public function preDispatch()
     {
@@ -14,6 +21,46 @@ class Registracion_RegistracionController extends Zend_Controller_Action
     }
     public function addAction()
     {
+        if ($this->_request->isPost()) {
+            Zend_Loader::loadClass('Zend_Filter_StripTags');
+            $filter 	= new Zend_Filter_StripTags();
+
+            $usuario	= trim($filter->filter($this->_request->getPost('username')));
+            $clave	= trim($filter->filter($this->_request->getPost('password')));
+            $mail	= trim($filter->filter($this->_request->getPost('email')));
+            $nombre	= trim($filter->filter($this->_request->getPost('name')));
+            $genero	= trim($filter->filter($this->_request->getPost('gender')));
+            $idZona	= trim($filter->filter($this->_request->getPost('state')));
+            $ciudad	= trim($filter->filter($this->_request->getPost('city')));
+            $dia	= trim($filter->filter($this->_request->getPost('day')));
+            $mes	= trim($filter->filter($this->_request->getPost('month')));
+            $ano	= trim($filter->filter($this->_request->getPost('year')));
+            $fechaNacimiento	=	"$ano-$mes-$dia";
+
+            if( $usuario != '' && $clave != '' && $mail != '') {
+                $data = array(
+                    'usuario'	=> $usuario,
+                    'clave'		=> $clave,
+                    'mail' 		=> $mail,
+                    'nombre' 	=> $nombre,
+                    'genero' 	=> $genero,
+                    'idZona' 	=> $idZona,
+                    'ciudad'	=> $ciudad,
+                    'fechaNacimiento' 	=> $fechaNacimiento,
+                    'estado'	=>	1,
+                    'creado'	=>	date("Y-m-d h:m:s")
+                );
+                $registracion = new Registracion();
+                $registracion->insert( $data );
+                $this->_redirect('/registracion/registracion/view');
+                return;
+            }
+            else{
+            	$this->view->message = "Todos los campos son necesarios";
+           		$this->_redirect('/registracion/registracion/');
+           }
+        }
+
     }
     public function uptadteAction()
     {
--- trunk/sql/1_USUARIOS.sql
+++ trunk/sql/1_USUARIOS.sql
@@ -1,20 +1,23 @@
 # Table "usuarios" DDL
 
 CREATE TABLE `usuarios` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `idUsuario` mediumint(10) unsigned NOT NULL auto_increment,
+  `idZona` mediumint(10) NOT NULL,
   `usuario` varchar(32) character set utf8 collate utf8_spanish_ci NOT NULL,
-  `password` varchar(32) character set utf8 collate utf8_spanish_ci NOT NULL,
+  `clave` varchar(32) NOT NULL,
   `nombre` varchar(40) character set utf8 collate utf8_spanish_ci NOT NULL,
   `apellido` varchar(40) character set utf8 collate utf8_spanish_ci NOT NULL,
-  `mail` varchar(64) character set utf8 collate utf8_spanish_ci default NULL,
-  `gender` enum('Mujer','Hombre') default NULL,
-  `country_s` varchar(50) default NULL,
-  `state` varchar(50) default NULL,
-  `city` varchar(50) default NULL,
+  `mail` varchar(64) NOT NULL,
+  `genero` enum('Mujer','Hombre') NOT NULL,
+  `ciudad` char(50) NOT NULL,
   `fechaNacimiento` date default NULL,
-  `estado` int(2) NOT NULL,
+  `estado` tinyint(1) NOT NULL,
   `creado` datetime NOT NULL,
   `modificado` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `usuario` (`usuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  PRIMARY KEY  (`idUsuario`),
+  UNIQUE KEY `usuario_idx` (`usuario`),
+  KEY `zona_idx` (`idZona`),
+  KEY `pass_idx` (`clave`),
+  KEY `nacimiento_idx` (`fechaNacimiento`),
+  KEY `estado_idx` (`estado`)
+) ENGINE=MyISAM
