91003471814ad4c587e3084c9760b5cb6edcef36
enriqueplace <enriqueplace@339f5665-9641-0410-b0e2-e15373425c37>
2008-06-05 00:46:42 +0000

    Se deja la última versión de la estructura de la base de datos

--- sql/00_INICIO.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE `surforce-social` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE `surforce-social`;
-
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
--- sql/07_STYLES_SELECTORES.sql
+++
@@ -1,9 +0,0 @@
-
-# Table "styles_selectores" DDL
-
-CREATE TABLE `styles_selectores` (
-  `id_selector` int(11) NOT NULL auto_increment,
-  `selector` varchar(64) collate utf8_spanish_ci NOT NULL,
-  `descripcion` varchar(255) collate utf8_spanish_ci default NULL,
-  PRIMARY KEY  (`id_selector`)
-) ENGINE=MyISAM;
