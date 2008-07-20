9c3afc14c82a89a192db73e40d9ccc1fd39a9500
enriqueplace <enriqueplace@339f5665-9641-0410-b0e2-e15373425c37>
2008-07-20 21:29:25 +0000

    Refactoring

--- branches/0.0.2/library/Zsurforce/modules/autenticacion/controllers/AutenticacionController.php
+++ branches/0.0.2/library/Zsurforce/modules/autenticacion/controllers/AutenticacionController.php
@@ -1,44 +1,50 @@
 <?php
-class Autenticacion_AutenticacionController extends Zend_Controller_Action {
-
-    function init(){ 
+class Autenticacion_AutenticacionController extends Zend_Controller_Action 
+{
+    public function init()
+    { 
         $this->initView();
         $this->view->baseUrl = $this->_request->getBaseUrl();
         $this->view->setScriptPath('./application/views/scripts/');
         $this->view->opcionMenu = 'autenticacion';
     }
-
-    function indexAction(){
-        
+    public function indexAction()
+    {        
         $this->_redirect('/autenticacion/autenticacion/login');
     }
-
-    function logoutAction(){
+    public function logoutAction()
+    {
         Zend_Auth::getInstance()->clearIdentity();
         $this->_redirect('/');
     }
-
-    function loginAction(){
+    public function loginAction()
+    {
         $info = Zend_Registry::get('personalizacion');
+        
         $this->view->message 				= '';
         $this->view->msgNombreVacio 		= $info->sitio->autenticacion->login->msgNombreVacio;
         $this->view->msgPasswordVacio 		= $info->sitio->autenticacion->login->msgPasswordVacio;
         $this->view->msgUserPassIncorrectos = $info->sitio->autenticacion->login->msgUserPassIncorrectos;
+        
         $tplJson = null;
+        
         if ($this->_request->isPost()) {
             Zend_Loader::loadClass('Zend_Filter_StripTags');
+
             $f 			= new Zend_Filter_StripTags();
             $usuario 	= $f->filter($this->_request->getPost('usuario'));
             $password 	= md5($f->filter($this->_request->getPost('password')));
 			$verificado = $f->filter($this->_request->getPost('verificado'))=='false'?false:true;
+			
             if (empty($usuario)) {
                 $this->view->message = $info->sitio->autenticacion->login->msgNombreVacio;
-            }
-            else {
+            }else{
 
                 Zend_Loader::loadClass('Zend_Auth_Adapter_DbTable');
+                
                 $dbAdapter  = Zend_Registry::get('dbAdapter');
                 $autAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
+                
                 $autAdapter->setTableName('usuarios');
                 $autAdapter->setIdentityColumn('usuario');
                 $autAdapter->setCredentialColumn('password');
@@ -48,26 +54,22 @@ class Autenticacion_AutenticacionController extends Zend_Controller_Action {
 
                 $aut 		= Zend_Auth::getInstance();
                 $result 	= $aut->authenticate($autAdapter);
+                
                 if(!$verificado){
                 	Zend_Loader::loadClass('Zend_Json');
                 	$this->view->varsJson = array('verificado'=> $result->isValid());
                 	$tplJson= 'autentificacion';
-                }
-                elseif ($result->isValid()) {
+                }elseif ($result->isValid()) {
                     $data 	= $autAdapter->getResultRowObject(null, 'password');
                     $aut->getStorage()->write($data);
                     $this->_redirect('/');
                 }
             }
         }
-		
-        
         $this->view->title 		= $info->sitio->autenticacion->login->titulo;
         $this->view->scriptJs 	= "scriptaculous";
+        
         $this->render($tplJson);
-
     }
-
-
 }
 ?>
